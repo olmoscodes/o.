02b23f0df822d714e99cbc13f970626afc6d1a02
Marc Olmos Cabezas <marcolmoscabezas@MacBook-Pro-de-Marc.local>
2022-12-03 12:20:08 +0100

    google analytics on preproduction mode turned off

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,26 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyA5sskrT0SvGJB_rmLZe8oltuQ1hfHjN9U",
-  authDomain: "olmoscodesportfolio.firebaseapp.com",
-  databaseURL: "https://olmoscodesportfolio.firebaseio.com",
-  projectId: "olmoscodesportfolio",
-  storageBucket: "olmoscodesportfolio.appspot.com",
-  messagingSenderId: "313664518227",
-  appId: "1:313664518227:web:e7a950c23c0ae0dd7ceea6",
-  measurementId: "G-SG8F7D3E31"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false
 };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
